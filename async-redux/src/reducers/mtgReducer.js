a56fedf147d710d90663455caf495095ffe28dee
Nicholas Myers <nicholas.myers.porfessional@gmail.com>
2020-05-13 12:54:37 -0700

    gbetting state and reducer set up with middleware logger and thunk

---
+++ async-redux/src/reducers/mtgReducer.js
@@ -0,0 +1,12 @@
+const initialState = {
+    isFetching: false,
+    data: [],
+    error: ""
+}
+
+export const mtgReducer = (state = initialState, action) => {
+    switch (action.type) {
+        default:
+            return state
+    }
+}
